b967c147ca437ed05eb80d51758551d8eb9195d8
David Brandt <padresmurfa@gmail.com>
2018-05-11 17:14:14 +0000

    bugfix in is-error

--- dist/validator.js
+++ dist/validator.js
@@ -35,7 +35,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function isWhiteSpace(params) {
-    if (/\S/.test(myString)) {
+    if (/\S/.test(params)) {
         // string is not empty and not just whitespace
         return false;
     }
